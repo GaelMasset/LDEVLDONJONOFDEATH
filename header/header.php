288f792311a8f4ea1c12da6e4533f9805e5ba9ca
Martin Bazoge <martin.bazoge@etu.unicaen.fr>
2024-11-05 11:22:19 +0100

    ajout de la navbar (pas encore de liens)

---
+++ header/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style_header.css">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Frijole&family=Pirata+One&display=swap');</style>
+    <title>Header</title>
+</head>
+<body>
+    <ul class="nav_bar">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li id="connex"><a href="#">Connexion</a></li>
+        <li hidden><a href="#">Compte</a><li>
+    </ul>
+    
+
+</body>
+</html>
