861720da409afb9d8e19de3f3d9f5c43ac8a9d88
rabute231 <antoinerabute@gmail.com>
2024-12-21 01:32:42 +0100

    pas d'erreurs, mais pas sur que ça enregistre bien

--- Vue/CreationPersonnage/traitementCharacter.php
+++ Vue/CreationPersonnage/traitementCharacter.php
@@ -1,7 +1,7 @@
 <?php
     //session_start();
     include_once('bdd.php');
-    require 'header/style_header.css';
+    //require 'header/style_header.css';
 
     if(!(isset($_POST['classe'])) || !(isset($_POST['nom'])) || !(isset($_POST['background']))){
         echo("<p>Quelque chose s'est mal passé</p>");
@@ -14,17 +14,21 @@
     }
 
     if($classe==1){
-        $pdo = new PDO($dsn, $user, $pass);
-        $requete = $pdo->prepare("insert into hero (name, image, biography, pv, mana, strength, initiative, xp, current_level, idCompte, current_chapter) 
-                                    values (.$classe., 'images/Berserker.jpg', .$background., 30, 0, 15, 5, 0, 1, .$_SESSION['id']. , 1)");
-        $requete->execute();
+        try{
+            $pdo = new PDO($dsn, $user, $pass);
+            $requete = $pdo->prepare("SELECT into hero (name, image, biography, pv, mana, strength, initiative, xp, current_level, idCompte, current_chapter) 
+                                        values (.$classe., 'images/Berserker.jpg', .$background., 30, 0, 15, 5, 0, 1, 2 , 1)");
+            $requete->execute();
+        }catch (PDOException $e) {
+        }
     }
     
 
     //TODO : 
     // faire la requête permettant de créer un nouveau personnage avec les données plus haut
+    // Une par classe
 
     //Problème : 
-    // il faut le lier a l'utilisateur, peut-être en utilisant le session
+    // il faut le lier a l'utilisateur en utilisant le $_SESSION
 
 ?>
