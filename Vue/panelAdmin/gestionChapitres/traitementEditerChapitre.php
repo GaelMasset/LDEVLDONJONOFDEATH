daebacad00ee01e0b0325c5d73a18d06e473996c
gael <gael.masset35@gmail.com>
2024-12-22 15:02:33 +0100

    choix du encounter dans la modif du chapitre

--- Vue/panelAdmin/gestionChapitres/traitementEditerChapitre.php
+++ Vue/panelAdmin/gestionChapitres/traitementEditerChapitre.php
@@ -16,6 +16,10 @@ try {
     $stmt = $pdo->prepare($sql);
     $stmt->execute(['id' => $id]);
 
+    $sql = "DELETE from encounter where chapter_id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['id' => $id]);
+
     echo $_POST['choixChapitre1'] . "     " . $_POST['choixChapitre1'];
     if($_POST['choixChapitre1'] != "/"){
         $sql = "INSERT into links (chapter_id, next_chapter_id, description) values (:nouveauId, :next_id, :description)";
@@ -36,6 +40,18 @@ try {
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['nouveauId' => $id, 'next_id' => $_POST['choixChapitre3'], 'description' => $_POST['desclien3']]);
     }
+
+    if($_POST['choixMonstreRencontre'] != "/"){
+        $sql = "SELECT * from monster where name = :name";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['name' => $_POST['choixMonstreRencontre']]);
+        $monstre = $stmt->fetch  (PDO::FETCH_ASSOC);
+
+        $sql = "INSERT into encounter (chapter_id, monster_id) values (:chapitre, :monstre_id)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['chapitre' => $id, 'monstre_id' => $monstre['id']]);
+    }
+
 
 
     header('Location: panelAdmin');
