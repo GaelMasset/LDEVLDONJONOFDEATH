e08b393b50a3e0586bc47b51374e65422bce070b
rabute231 <antoinerabute@gmail.com>
2024-12-24 16:20:47 +0100

    ajout du js pour la séléction de la classe

---
+++ Vue/CreationPersonnage/CreationPersonnageManager.js
@@ -0,0 +1,6 @@
+let guerrier = document.getElementById("imgGuerrier");
+let  mage = document.getElementById("imgMage");
+let voleur = document.getElementById("imgVoleur");
+option.addEventListener("click", async () =>
+    
+);
