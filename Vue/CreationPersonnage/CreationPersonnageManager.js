4e34f7dc84a0ea577789e82c1e52cdb200960286
Didisdepix <119625094+Didisdepix@users.noreply.github.com>
2024-12-26 17:40:48 +0100

    Update fait en voiture

--- Vue/CreationPersonnage/CreationPersonnageManager.js
+++ Vue/CreationPersonnage/CreationPersonnageManager.js
@@ -1,6 +1,6 @@
-let guerrier = document.getElementById("imgGuerrier");
-let  mage = document.getElementById("imgMage");
-let voleur = document.getElementById("imgVoleur");
-option.addEventListener("click", async () =>
+let guerrier = document.getElementById("optionGuerrier");
+let mage = document.getElementById("optionMage");
+let voleur = document.getElementById("optionVoleur");
+guerrier.addEventListener("click", async () =>
     
 );
