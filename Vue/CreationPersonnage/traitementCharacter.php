18b02b525df232ab09a26b71bb72d3cb8bdb4bfc
rabute231 <antoinerabute@gmail.com>
2024-12-27 22:25:37 +0100

    création de perso fonctionnelle

--- Vue/CreationPersonnage/traitementCharacter.php
+++ Vue/CreationPersonnage/traitementCharacter.php
@@ -9,8 +9,8 @@
         $classe = $_POST['classe'];
         $nom = $_POST['nom'];
         $background = $_POST['background'];
-        //Pour une raison étrange
-        $idSession = $_SESSION['username'];
+        //Pour une raison étrange, le $_SESSION de id est vide mais pas les autres, donc on va faire une requête avec le username pour retrouver l'id
+        $username = $_SESSION['username'];
     }
 
     if($classe==1){
@@ -25,13 +25,21 @@
             // Récupérer le résultat et le stocker dans une variable
             $id = $stmt->fetchColumn() + 1;
 
-            $requete = $pdo->prepare("INSERT into hero (id,name, image, biography, pv, mana, strength, initiative, xp, current_level,  current_chapter, primary_weapon, idCompte) 
+            //Récupérer l'id de l'utilisateur
+            $sql = "SELECT id from compte where pseudo= :username";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['username' => $username]);
+
+            $idSession = $stmt->fetchColumn();
+
+            $requete = $pdo->prepare("INSERT into hero (id,name, class_id, image, biography, pv, mana, strength, initiative, xp, current_level,  current_chapter, primary_weapon, idCompte) 
                                         values (:id, :nom, :classe, 'images/Berserker.jpg', :background, 30, 0, 15, 5, 0, 1, 1,2, :idSession)");
             $requete->execute(['id' => $id, 'nom' => $nom, 'classe' => $classe, 'background'=>$background, 'idSession' => $idSession]);
 
             header('Location: profile');
             exit();
         }catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
 
@@ -47,13 +55,21 @@
             // Récupérer le résultat et le stocker dans une variable
             $id = $stmt->fetchColumn() + 1;
 
-            $requete = $pdo->prepare("INSERT into hero (id,name, image, biography, pv, mana, strength, initiative, xp, current_level, spell_list,  current_chapter, primary_weapon, idCompte) 
+            //Récupérer l'id de l'utilisateur
+            $sql = "SELECT id from compte where pseudo= :username";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['username' => $username]);
+
+            $idSession = $stmt->fetchColumn();
+
+            $requete = $pdo->prepare("INSERT into hero (id,name, class_id, image, biography, pv, mana, strength, initiative, xp, current_level, spell_list,  current_chapter, primary_weapon, idCompte) 
                                         values (:id, :nom ,:classe, 'images/Magician02.jpg', :background, 10, 30, 5, 10, 0, 1, 'Boule de feu, Soin mineure', 1, 4, :idSession)");
             $requete->execute(['id' => $id, 'nom' => $nom, 'classe' => $classe, 'background'=>$background, 'idSession' => $idSession]);
 
             header('Location: profile');
             exit();
         }catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
 
@@ -69,13 +85,21 @@
             // Récupérer le résultat et le stocker dans une variable
             $id = $stmt->fetchColumn() + 1;
 
-            $requete = $pdo->prepare("INSERT into hero (id,name, image, biography, pv, mana, strength, initiative, xp, current_level,  current_chapter, primary_weapon, idCompte) 
+            //Récupérer l'id de l'utilisateur
+            $sql = "SELECT id from compte where pseudo= :username";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['username' => $username]);
+
+            $idSession = $stmt->fetchColumn();
+
+            $requete = $pdo->prepare("INSERT into hero (id,name, class_id, image, biography, pv, mana, strength, initiative, xp, current_level,  current_chapter, primary_weapon, idCompte) 
                                         values (:id, :nom, :classe, 'images/Thief.jpg', :background, 20, 0, 10, 20, 0, 1,  1, 3, :idSession)");
             $requete->execute(['id' => $id, 'nom' => $nom, 'classe' => $classe, 'background'=>$background, 'idSession' => $idSession]);
 
             header('Location: profile');
             exit();
         }catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
 
